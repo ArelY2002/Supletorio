b007910afbe9de2871d25a740bbe5bdfcf682c0b
Zambrano Zambrano Arely <86209266+ArelY2002@users.noreply.github.com>
2021-07-30 13:09:19 -0500

    C++que permita ingresar precio y producto art10
    Buenas tardes ingeniero un cordial saludo de mi parte le saluda la alumna Zambrano Zambrano Arely de "1" "A" de "TIC"
    el trabajo consiste en hacer un programa que me permita ingresar el nombre y el precio de 10 productos, y lo hice de la siguiente forma:
    utilice  string artículos[10]; este me permite ingresar  caracteres.
     Ingrese precios, suma ,IVA, descuento y total que me va a permitir utilizarlos durante el proceso.
    primeramente utilice un  for en el cual hice el proceso de ingresar el articulo numérico, en este proceso utilice el articulo en el cual fue llamado con string , en este mismo for ingrese el precio de los 10 artículos.
    Para este proceso utilice suma +=precios[x]; que me va a permitir sumar el precio de los artículos, también calculamos el iva con iva=suma*0.12;  luego calculamos el descuento con  des=suma*0.10; y el total con total=(suma+iva)-des;
    system("cls"); esto me permite limpiar la pantalla ya que queremos que el usuario no vea los datos que les asignamos previamente.
    Luego con cout ingrese los mensajes que van a una factura , por ejemplo nombre del proveedor y el nombre del dueño de donde esta vendiendo el producto, la dirección, numero telefónico y convencional entre otras cosas.
    Por ultimo utilice un for que me permite hacer el proceso de mostrar la cantidad de artículos y precios del mismo.
    Para culminar este proceso utilice cout para presentar por pantalla un mensaje y el resultado de cada variable:
    presentar el resultado de la suma , el IVA, el descuento y el total.

--- SUPLETORIO 1.cpp
+++ SUPLETORIO 1.cpp
@@ -3,6 +3,29 @@ using namespace std;
 int main(){
  string	articulos[10];
  float precios[10],suma,iva,des,total;
- 
+ 	for(int x=0;x<10;x++){
+		cout<<"Ingrese el articulo numero "<<x+1<<endl;
+		cin>>articulos[x];
+		//getline (cin,articulos[x]);
+		cout<<"Ingrese el precios "<<x+1<<endl;
+		cin>>precios[x];
+		suma +=precios[x];
+		iva=suma*0.12;
+		des=suma*0.10;
+		total=(suma+iva)-des;
+	}
+	system("cls");
+	cout<<"FACTURERO DE ARELY ZAMBRANO"<<endl;
+	cout<<"ARELY ZAMBRANO ZAMBRANO"<<endl;
+	cout<<"MATRIZ: VICHE/ESMERALDAS"<<endl;
+	cout<<"TELEFONOS: 0985628786/062454315"<<endl;
+	cout<<"VICHE/ECUADOR"<<endl;
+	for(int x=0;x<10;x++){
+		cout<<x+1<<"."<<articulos[x]<<"               "<<"\t\t\t\t"<<precios[x]<<endl;
+	}
+	cout<<"SUBTOTAL $"<<suma<<endl;
+	cout<<"IVA 12% $"<<iva<<endl;
+	cout<<"DESCUENTO 10% $"<<des<<endl;
+	cout<<"TOTAL A PAGAR $"<<total;
 }
 
